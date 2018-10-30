ac197308f45133b8785292e6e49a001f375ce98e
420coupe <420coupe@Gmail.com>
2018-10-30 16:09:04 -0400

    SiaPrimeStats.com

--- web/yaamp/core/rpc/rpc.php
+++ web/yaamp/core/rpc/rpc.php
@@ -5,9 +5,4 @@ require_once('json-rpc.php');
 require_once('ethereum.php');
 require_once('xmr-rpc.php');
 require_once('wallet-rpc.php');
-<<<<<<< HEAD
 require_once('siaprime.php');
-=======
-require_once('sia.php');
-require_once('siaprime.php');
->>>>>>> 507632959f53264c2ea8218809ac5dcf74b35f38
